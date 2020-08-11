a003321c7a9f1d5a5a9b85adec347becb258c8c1
Panchajanya1999 <panchajanya@azure-dev.live>
2020-08-11 17:29:12 +0530

    build-tc: Build with LTO && PGO
    Change-Id: I196062dd5726a6c2c65e54c79d52a821458c582c
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- build-tc.sh
+++ build-tc.sh
@@ -49,6 +49,8 @@ tg_post_msg "<code>Building LLVM</code>"
 	--targets "ARM;AArch64" \
 	--shallow-clone \
 	--incremental \
+	--pgo \
+	--lto full \
 	--build-type "Release" 2>&1 | tee build.log
 
 # Check if the final clang binary exists or not.
