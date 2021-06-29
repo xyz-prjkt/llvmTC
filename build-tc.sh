3966ea6b5c7dae06cf3ef0c759b471765c24f052
xyzuan <xyzuan@webmail.umm.ac.id>
2021-06-29 12:38:07 +0700

    xRageChain: initial.

--- build-tc.sh
+++ build-tc.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 
+sudo apt-get install -y --no-install-recommends \
+        bc \
+        bison \
+        ca-certificates \
+        clang \
+        cmake \
+        curl \
+        file \
+        flex \
+        gcc \
+        g++ \
+        git \
+        libelf-dev \
+        libssl-dev \
+        lld \
+        make \
+        ninja-build \
+        python3 \
+        texinfo \
+        xz-utils \
+        zlib1g-dev
+
 # Function to show an informational message
 msg() {
     echo -e "\e[1;32m$*\e[0m"
@@ -10,15 +32,16 @@ err() {
 }
 
 # Set Chat ID, to push Notifications
-CHATID="-1001231303646"
+CHATID="-1001389519102"
 
 # Set a directory
 DIR="$(pwd ...)"
 
 # Inlined function to post a message
-export BOT_MSG_URL="https://api.telegram.org/bot$TOKEN/sendMessage"
+export BOT_MSG_URL="https://api.telegram.org/bot$TG_TOKEN/sendMessage"
+export BOT_BUILD_URL="https://api.telegram.org/bot$TG_TOKEN/sendMessage"
 tg_post_msg() {
-	curl -s -X POST "$BOT_MSG_URL" -d chat_id="$CHATID" \
+	curl -s -X POST "$BOT_MSG_URL" -d chat_id="$TG_CHAT_ID" \
 	-d "disable_web_page_preview=true" \
 	-d "parse_mode=html" \
 	-d text="$1"
@@ -26,7 +49,7 @@ tg_post_msg() {
 }
 tg_post_build() {
 	curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
-	-F chat_id="$2"  \
+	-F chat_id="$TG_CHAT_ID"  \
 	-F "disable_web_page_preview=true" \
 	-F "parse_mode=html" \
 	-F caption="$3"
@@ -38,13 +61,13 @@ rel_friendly_date="$(date "+%B %-d, %Y")" # "Month day, year" format
 builder_commit="$(git rev-parse HEAD)"
 
 # Send a notificaton to TG
-tg_post_msg "<b>Azure Clang Compilation Started</b>%0A<b>Date : </b><code>$rel_friendly_date</code>%0A<b>Toolchain Script Commit : </b><code>$builder_commit</code>%0A"
+tg_post_msg "<b>xRageChain-tc Compilation Started</b>%0A<b>Date : </b><code>$rel_friendly_date</code>%0A<b>Toolchain Script Commit : </b><code>$builder_commit</code>%0A"
 
 # Build LLVM
 msg "Building LLVM..."
 tg_post_msg "<code>Building LLVM</code>"
 ./build-llvm.py \
-	--clang-vendor "Azure" \
+	--clang-vendor "xRageChain-tc" \
 	--projects "clang;lld;polly" \
 	--targets "ARM;AArch64" \
 	--shallow-clone \
@@ -64,6 +87,42 @@ tg_post_msg "<code>Building LLVM</code>"
 msg "Building binutils..."
 tg_post_msg "<code>Building Binutils</code>"
 ./build-binutils.py --targets arm aarch64
+
+# build gcc
+PREFIX=install/
+msg "Downloading GCC source code"
+git clone https://git.linaro.org/toolchain/gcc.git -b master gcc --depth=1
+
+build_gcc () {
+    msg "Building GCC"
+    cd gcc
+    ./contrib/download_prerequisites
+    cd ../
+    mkdir build-gcc
+    cd build-gcc
+    ../gcc/configure --target=arm64 \
+                     --prefix="$PREFIX" \
+                     --disable-decimal-float \
+                     --disable-libffi \
+                     --disable-libgomp \
+                     --disable-libmudflap \
+                     --disable-libquadmath \
+                     --disable-libstdcxx-pch \
+                     --disable-nls \
+                     --disable-shared \
+                     --disable-docs \
+                     --enable-default-ssp \
+                     --enable-languages=c,c++ \
+                     --with-pkgversion="xRageChain GCC" \
+                     --with-newlib \
+                     --with-gnu-as \
+                     --with-gnu-ld \
+                     --with-sysroot
+    make CFLAGS="-flto -O3 -pipe -ffunction-sections -fdata-sections" CXXFLAGS="-flto -O3 -pipe -ffunction-sections -fdata-sections" all-gcc -j$(nproc --all)
+    make CFLAGS="-flto -O3 -pipe -ffunction-sections -fdata-sections" CXXFLAGS="-flto -O3 -pipe -ffunction-sections -fdata-sections" all-target-libgcc -j$(nproc --all)
+    make install-gcc -j$(nproc --all)
+    make install-target-libgcc -j$(nproc --all)
+}
 
 # Remove unused products
 rm -fr install/include
@@ -93,24 +152,24 @@ llvm_commit_url="https://github.com/llvm/llvm-project/commit/$short_llvm_commit"
 binutils_ver="$(ls | grep "^binutils-" | sed "s/binutils-//g")"
 clang_version="$(install/bin/clang --version | head -n1 | cut -d' ' -f4)"
 
-tg_post_msg "<b>Azure clang compilation Finished</b>%0A<b>Clang Version : </b><code>$clang_version</code>%0A<b>LLVM Commit : </b><code>$llvm_commit_url</code>%0A<b>Binutils Version : </b><code>$binutils_ver</code>"
+tg_post_msg "<b>xRageChain-tc compilation Finished</b>%0A<b>Clang Version : </b><code>$clang_version</code>%0A<b>LLVM Commit : </b><code>$llvm_commit_url</code>%0A<b>Binutils Version : </b><code>$binutils_ver</code>"
 
 # Push to GitHub
 # Update Git repository
-git config --global user.name "Panchajanya1999"
-git config --global user.email "panchajanya@azure-dev.live"
-git clone "https://Panchajanya1999:$GITHUB_TOKEN@github.com/Panchajanya1999/azure-clang.git" rel_repo
+git config --global user.name "xyzuan"
+git config --global user.email "xyzuan@webmail.umm.ac.id"
+git clone "https://xyzuan:$GH_TOKEN@github.com/xyz-prjkt/xRageChain-tc_build.git" rel_repo
 pushd rel_repo || exit
 rm -fr ./*
 cp -r ../install/* .
 git checkout README.md # keep this as it's not part of the toolchain itself
 git add .
-git commit -asm "Update to $rel_date build
+git commit -asm "Bump to $rel_date build
 
 LLVM commit: $llvm_commit_url
 Clang Version: $clang_version
 Binutils version: $binutils_ver
-Builder commit: https://github.com/Panchajanya1999/tc-build/commit/$builder_commit"
+Builder commit: https://github.com/xyz-prjkt/xRageChain-tc_build/commit/$builder_commit"
 git push -f
 popd || exit
 tg_post_msg "<b>Toolchain Compilation Finished and pushed</b>"
