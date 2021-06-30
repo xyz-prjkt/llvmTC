6443a371e30f8a3bfc4e366093e649c15ddf67f4
Jody Yuantoro <i.xyzuan@gmail.com>
2021-06-30 15:50:38 +0700

    xRageChain: push git commit changes

--- build-tc.sh
+++ build-tc.sh
@@ -104,7 +104,7 @@ rm -fr ./*
 cp -r ../install/* .
 git checkout README.md # keep this as it's not part of the toolchain itself
 git add .
-git commit -asm "Bump to $rel_date build
+git commit -asm "xRageChain: Bump to $rel_date build
 
 LLVM commit: $llvm_commit_url
 Clang Version: $clang_version
