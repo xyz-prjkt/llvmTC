e28e60e41e64095473fb54f2ebdc5afe73fe428a
Jody Yuantoro <xyzuannihboss@gmail.com>
2021-07-06 19:41:12 +0700

    llvm-tc: nuke LTO Thin

--- build-tc.sh
+++ build-tc.sh
@@ -54,7 +54,6 @@ tg_post_msg "<b>$LLVM_NAME: Building LLVM. . .</b>"
 	--projects "clang;lld;polly" \
 	--targets "ARM;AArch64" \
 	--shallow-clone \
-	--lto thin \
 	--incremental \
 	--build-type "Release" 2>&1 | tee build.log
 
