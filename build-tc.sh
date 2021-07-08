b9fdddb6a7a64a3e7e80f0dbf5e766115f5df063
Jody Yuantoro <xyzuannihboss@gmail.com>
2021-07-08 10:19:34 +0700

    build-tc: initial local build

--- build-tc.sh
+++ build-tc.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-# Secret Variable for CI
-# LLVM_NAME | Your desired Toolchain Name
-# TG_TOKEN | Your Telegram Bot Token
-# TG_CHAT_ID | Your Telegram Channel / Group Chat ID
-# GH_USERNAME | Your Github Username
-# GH_EMAIL | Your Github Email
-# GH_TOKEN | Your Github Token ( repo & repo_hook )
-# GH_PUSH_REPO_URL | Your Repository for store compiled Toolchain ( without https:// or www. ) ex. github.com/xyz-prjkt/xRageTC.git
+# Main
+LLVM_NAME= # Your desired Toolchain Name
+GH_USERNAME= # Your Github Username
+GH_EMAIL= # Your Github Email
+GH_TOKEN= # Your Github Token ( repo & repo_hook )
+GH_PUSH_REPO_URL= # Your Repository for store compiled Toolchain ( without https:// or www. ) ex. github.com/xyz-prjkt/xRageTC.git
 
 # Function to show an informational message
 msg() {
@@ -21,36 +19,19 @@ err() {
 # Set a directory
 DIR="$(pwd ...)"
 
-# Inlined function to post a message
-export BOT_MSG_URL="https://api.telegram.org/bot$TG_TOKEN/sendMessage"
-tg_post_msg() {
-	curl -s -X POST "$BOT_MSG_URL" -d chat_id="$TG_CHAT_ID" \
-	-d "disable_web_page_preview=true" \
-	-d "parse_mode=html" \
-	-d text="$1"
-
-}
-tg_post_build() {
-	curl --progress-bar -F document=@"$1" "$BOT_MSG_URL" \
-	-F chat_id="$TG_CHAT_ID"  \
-	-F "disable_web_page_preview=true" \
-	-F "parse_mode=html" \
-	-F caption="$3"
-}
-
 # Build Info
 rel_date="$(date "+%Y%m%d")" # ISO 8601 format
 rel_friendly_date="$(date "+%B %-d, %Y")" # "Month day, year" format
 builder_commit="$(git rev-parse HEAD)"
 
-# Send a notificaton to TG
-tg_post_msg "<b>$LLVM_NAME: Toolchain Compilation Started</b>%0A<b>Date : </b><code>$rel_friendly_date</code>%0A<b>Toolchain Script Commit : </b><code>$builder_commit</code>%0A"
+# Send information
+msg "<b>${LLVM_NAME}: Toolchain Compilation Started</b>%0A<b>Date : </b><code>$rel_friendly_date</code>%0A<b>Toolchain Script Commit : </b><code>$builder_commit</code>%0A"
 
 # Build LLVM
-msg "$LLVM_NAME: Building LLVM..."
-tg_post_msg "<b>$LLVM_NAME: Building LLVM. . .</b>"
+msg "${LLVM_NAME}: Building LLVM..."
+tg_post_msg "<b>${LLVM_NAME}: Building LLVM. . .</b>"
 ./build-llvm.py \
-	--clang-vendor "$LLVM_NAME" \
+	--clang-vendor "${LLVM_NAME}" \
 	--projects "clang;lld;polly" \
 	--targets "ARM;AArch64" \
 	--shallow-clone \
@@ -60,13 +41,13 @@ tg_post_msg "<b>$LLVM_NAME: Building LLVM. . .</b>"
 # Check if the final clang binary exists or not.
 [ ! -f install/bin/clang-1* ] && {
 	err "Building LLVM failed ! Kindly check errors !!"
-	tg_post_build "build.log" "$TG_CHAT_ID" "Error Log"
+	err "build.log" "Error Log"
 	exit 1
 }
 
 # Build binutils
-msg "$LLVM_NAME: Building binutils..."
-tg_post_msg "<b>$LLVM_NAME: Building Binutils. . .</b>"
+msg "${LLVM_NAME}: Building binutils..."
+msg "<b>${LLVM_NAME}: Building Binutils. . .</b>"
 ./build-binutils.py --targets arm aarch64
 
 # Remove unused products
@@ -97,24 +78,24 @@ llvm_commit_url="https://github.com/llvm/llvm-project/commit/$short_llvm_commit"
 binutils_ver="$(ls | grep "^binutils-" | sed "s/binutils-//g")"
 clang_version="$(install/bin/clang --version | head -n1 | cut -d' ' -f4)"
 
-tg_post_msg "<b>$LLVM_NAME: Toolchain compilation Finished</b>%0A<b>Clang Version : </b><code>$clang_version</code>%0A<b>LLVM Commit : </b><code>$llvm_commit_url</code>%0A<b>Binutils Version : </b><code>$binutils_ver</code>"
+msg "<b>${LLVM_NAME}: Toolchain compilation Finished</b>%0A<b>Clang Version : </b><code>$clang_version</code>%0A<b>LLVM Commit : </b><code>$llvm_commit_url</code>%0A<b>Binutils Version : </b><code>$binutils_ver</code>"
 
 # Push to GitHub
 # Update Git repository
-git config --global user.name $GH_USERNAME
-git config --global user.email $GH_EMAIL
-git clone "https://$GH_USERNAME:$GH_TOKEN@$GH_PUSH_REPO_URL" rel_repo
+git config --global user.name ${GH_USERNAME}
+git config --global user.email ${GH_EMAIL}
+git clone "https://${GH_USERNAME}:${GH_TOKEN}@${GH_PUSH_REPO_URL}" rel_repo
 pushd rel_repo || exit
 rm -fr ./*
 cp -r ../install/* .
 git checkout README.md # keep this as it's not part of the toolchain itself
 git add .
-git commit -asm "$LLVM_NAME: Bump to $rel_date build
+git commit -asm "${LLVM_NAME}: Bump to $rel_date build
 
 LLVM commit: $llvm_commit_url
 Clang Version: $clang_version
 Binutils version: $binutils_ver
-Builder commit: https://$GH_PUSH_REPO_URL/commit/$builder_commit"
+Builder commit: https://${GH_PUSH_REPO_URL}/commit/$builder_commit"
 git push -f
 popd || exit
-tg_post_msg "<b>$LLVM_NAME: Toolchain pushed to <code>https://$GH_PUSH_REPO_URL</code></b>"
+msg "<b>${LLVM_NAME}: Toolchain pushed to <code>https://${GH_PUSH_REPO_URL}</code></b>"
